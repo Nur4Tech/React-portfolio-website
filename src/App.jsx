1868aa4f093cd3332b31671e3db21774d02b4a5d
Rami <no.one1.iq@gmail.com>
2024-07-07 21:25:09 +0400

    Recat App

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { Header, Hero, Main, Contact, Footer } from "./components/index";
+import { useEffect, useState } from "react";
+
+function App() {
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 300) {
+        setshowScrollBTN(true);
+      } else {
+        setshowScrollBTN(false);
+      }
+    });
+  }, []);
+
+  const [showScrollBTN, setshowScrollBTN] = useState(false);
+  return (
+    <div id="up" className="container">
+      <Header />
+
+      <Hero />
+      <div className="divider" />
+      <Main />
+      <div className="divider" />
+      <Contact />
+      <div className="divider" />
+      <Footer />
+      <a
+        style={{ opacity: showScrollBTN ? 1 : 0, transition: "1s" }}
+        href="#up"
+      >
+        <button className="icon-keyboard_arrow_up scroll2Top"></button>
+      </a>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/index.jsx
@@ -0,0 +1,5 @@
+export {default as Header} from './1-header/Header'
+export {default as Hero} from './2-hero/Hero'
+export {default as Main} from './3-main/Main'
+export {default as Contact} from './4-contact/Contact'
+export {default as Footer} from './5-footer/Footer'
